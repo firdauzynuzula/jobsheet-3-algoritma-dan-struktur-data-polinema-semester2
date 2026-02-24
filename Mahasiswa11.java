c723646b01089a286e7a81a8607e15aa5d27f44b
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-02-24 09:11:00 +0700

    first commit

---
+++ Mahasiswa11.java
@@ -0,0 +1,11 @@
+public class Mahasiswa11 {
+    public static void main(String[] args) {
+        String nama = "John Doe";
+        int umur = 20;
+        String jurusan = "Teknik Informatika";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
