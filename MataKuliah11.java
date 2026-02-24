c33cf10f9add81b4ea4bc9e43ad0f959c0f21be5
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-02-24 11:17:05 +0700

    mengubah file MataKuliahDemo11.java dan MataKuliah11.java

--- MataKuliah11.java
+++ MataKuliah11.java
@@ -1,3 +1,14 @@
 public class MataKuliah11 {
     
+    public String kode;
+    public String nama;
+    public int sks;
+    public int JumlahJam;
+
+    public MataKuliah11(String kode, String nama, int sks, int JumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.JumlahJam = JumlahJam;
+    }
 }
--- MataKuliahDemo11.java
+++ MataKuliahDemo11.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;   
 public class MataKuliahDemo11 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        MataKuliah11[] arrayOfMataKuliah = new MataKuliah11[3];
+
+        String kode, nama, dummy;
+        int sks, jumlahJam;
         
+      for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan data untuk Mata Kuliah ke-" + (i + 1));
+            System.out.print("Kode: ");
+            kode = input.nextLine();
+            System.out.print("Nama: ");
+            nama = input.nextLine();
+            System.out.print("Sks: ");
+            dummy = input.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam: ");
+            dummy = input.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            
+            arrayOfMataKuliah[i] = new MataKuliah11(kode, nama, sks, jumlahJam);
+            System.out.println("-----------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nData Mata Kuliah ke-" + (i + 1));
+            System.out.println("Kode: " + arrayOfMataKuliah[i].kode);
+            System.out.println("Nama: " + arrayOfMataKuliah[i].nama);
+            System.out.println("SKS: " + arrayOfMataKuliah[i].sks);
+            System.out.println("Jumlah Jam: " + arrayOfMataKuliah[i].JumlahJam);
+            System.out.println("-----------------------------");
+        }
+        input.close();
     }
 }
